02e56677ed48e03d1debdc5de30d3bf8917f06f6
baurzhan-kenzhemukhanov <baurzhan.kenzhemukhanov@narxoz.kz>
2025-03-07 19:26:16 +0500

    Create ConfigManagerDemo.java

---
+++ src/singleton/ConfigManagerDemo.java
@@ -0,0 +1,14 @@
+package singleton;
+
+public class ConfigManagerDemo {
+    public static void main(String[] args) {
+        ConfigurationManager configManager = ConfigurationManager.getInstance();
+
+        System.out.println("Max Players: " + configManager.getConfig("maxPlayers"));
+        System.out.println("Default Language: " + configManager.getConfig("defaultLanguage"));
+        System.out.println("Game Difficulty: " + configManager.getConfig("gameDifficulty"));
+
+        System.out.println("\nAll Configuration Settings:");
+        configManager.printAllConfigs();
+    }
+}
